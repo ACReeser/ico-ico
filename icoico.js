0897aaedd807143b8f3aa6055cf2715420234301
OpenShift guest <rogueconformist@gmail.com>
2013-05-10 01:07:24 -0400

    konami arrows and cursor

--- icoico.js
+++ icoico.js
@@ -5,7 +5,57 @@ window.onload = function(){
         height: 512
       });
       var layer = new Kinetic.Layer();
+      var glass = new Kinetic.Layer();
+      glass.setListening(false);
 
+    var icoVisual = function(x, y, string, fontSize){
+        return new Kinetic.Text({
+          x: x,
+          y: y,
+          fontFamily: 'fontello',
+          text:string,
+          fontSize: fontSize||30,
+          fill:'black',
+        });
+    }
+    this.stateArrow = function(x, y, state){
+        this.state = state;
+        this.stateStrings = ["\u21c8", "\u21c9", "\u21ca", "\u21c7"];
+        this.visual = new icoVisual(x, y, this.stateStrings[state]);
+        var that = this;
+        this.visual.on('mousedown', function(){ 
+            that.changeState(1);});
+        this.visual.on('mouseenter', function(){ emotCursor.setFill('orange');});
+        this.visual.on('mouseout', function(){ emotCursor.setFill('black');});
+    }
+    this.stateArrow.prototype.changeState = function(adjustment){
+        this.state += adjustment;
+        if (this.state > 3){
+            this.state = 0;
+        } else if (this.state < 0){
+            this.state = 3;
+        }
+        this.visual.setText(this.stateStrings[this.state]);
+    }
+    
+    var initStage1 = function(stage, layer){
+      var k1 = new stateArrow(stage.getWidth()/4, 50, 1);
+      var k2 = new stateArrow(stage.getWidth()/4 +50, 50, 2);
+      var k3 = new stateArrow(stage.getWidth()/4 +100, 50, 3);
+      var k4 = new stateArrow(stage.getWidth()/4 +150, 50, 0);
+      var k5 = new stateArrow(stage.getWidth()/4 +200, 50, 1);
+      var k6 = new stateArrow(stage.getWidth()/4 +250, 50, 2);
+      var k7 = new stateArrow(stage.getWidth()/4 +300, 50, 3);
+      var k8 = new stateArrow(stage.getWidth()/4 +350, 50, 0);
+      layer.add(k1.visual);
+      layer.add(k2.visual);
+      layer.add(k3.visual);
+      layer.add(k4.visual);
+      layer.add(k5.visual);
+      layer.add(k6.visual);
+      layer.add(k7.visual);
+      layer.add(k8.visual);
+    };
       //var simpleText = new Kinetic.Text({
     //    x: stage.getWidth() / 2,
       //  y: 15,
@@ -16,13 +66,22 @@ window.onload = function(){
      // });
      
      var emot = new Kinetic.Text({
-        x: stage.getWidth() / 2,
-        y: 15,
+        x: stage.getWidth() / 2 +15,
+        y: 30,
         fontFamily: 'fontello',
         text: '\uE800',
         fontSize: 18,
         fill: 'black',
         name:'emot',
+      });
+      var emotCursor = new Kinetic.Text({
+        x: stage.getWidth() / 2,
+        y: 15,
+        fontFamily: 'fontello',
+        text: '\u26A0',
+        fontSize: 18,
+        fill: 'black',
+        name:'cursor',          
       });
       
       emot.oncollide = function(collider){
@@ -49,9 +108,11 @@ window.onload = function(){
       };
       
       var moveEmot = function(mouseoverEvt){
-          emot.setX( mouseoverEvt.offsetX);
-          emot.setY( mouseoverEvt.offsetY);
-          emot.getLayer().draw();          
+          emotCursor.setX(mouseoverEvt.offsetX);
+          emotCursor.setY(mouseoverEvt.offsetY);
+          emot.setX( mouseoverEvt.offsetX+15);
+          emot.setY( mouseoverEvt.offsetY+15);
+          emot.getStage().draw();          
       };
       
       $('#icoCanvas').mouseover(moveEmot);
@@ -90,7 +151,10 @@ window.onload = function(){
       //#endregion
       
       // add the shapes to the layer
-      layer.add(emot);
       //layer.add(rect);
       stage.add(layer);
+      stage.add(glass);
+      initStage1(stage, layer);
+      glass.add(emotCursor);
+      layer.add(emot);
 };
